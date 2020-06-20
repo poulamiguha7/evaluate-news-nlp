c8b5903a2ae88696b5f7c384ce8471704068f8af
Mohga <38896263+MohgaEmam@users.noreply.github.com>
2020-06-20 07:22:14 +0200

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,13 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
@@ -17,27 +17,20 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-           }
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                }
         ]
-    },
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
-            filename: "./index.html",
+            filename: "./index.html"
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
+        new MiniCssExtractPlugin({ 
+            filename: "[name].css" 
         })
-    ]
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        }
 }
