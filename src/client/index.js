a76c3de61a19646eb38c19eb504c7c643d2142b6
poulamiguha7 <61988946+poulamiguha7@users.noreply.github.com>
2020-06-21 14:51:30 -0500

    Updated import statements

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { handleSubmit } from './js/formHandler';
 import { getUserData } from './js/getUserData';
 import { updateUI } from './js/updateUI';
 
+
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/footer.scss';
